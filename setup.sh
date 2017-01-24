93c4cdbc32de13ac9b6778b748e85b87db01ef68
Ryota Ikezawa <ikezawa-ryota@c-fo.com>
2017-01-24 17:05:33 +0900

    fix script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.zsh.d/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.zshenv ~/.zshenv
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/Dropbox/dev/Brewfile ~/Brewfile
